13d3b4c2817108267c95fc317e0b2e31baf132af
talonmyburgh <myburgh.talon@gmail.com>
2026-03-05 15:12:49 +0200

    ** update __init__ to match

---
+++ src/jackknify/__init__.py
@@ -0,0 +1,5 @@
+"""A Python-based package for jackknifing Measurement Set visibilities to create noise realisations"""
+
+__version__ = "0.2.0"
+
+__all__ = ["__version__"]
--- src/jackknify/cli/__init__.py
+++ src/jackknify/cli/__init__.py
@@ -1,9 +1,9 @@
 import typer
 
-from .make_test_ms import make_ms
-from .noise import noise
-from .onboard import onboard
-from .realise import realise
+from jackknify.cli.make_test_ms import make_ms
+from jackknify.cli.noise import noise
+from jackknify.cli.onboard import onboard
+from jackknify.cli.realise import realise
 
 app = typer.Typer(
     name="jackknify",
@@ -11,7 +11,16 @@ app = typer.Typer(
     no_args_is_help=True,
 )
 
+
+@app.callback()
+def callback() -> None:
+    """Jackknife interferometric datasets using JAX."""
+    pass
+
+
 app.command(name="realise")(realise)
 app.command(name="noise")(noise)
 app.command(name="make-test-ms")(make_ms)
 app.command(name="onboard")(onboard)
+
+__all__ = ["app"]
