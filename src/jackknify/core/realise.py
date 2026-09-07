dea5283a8509358f7a9bd06b6cedacb1507c633f
talonmyburgh <myburgh.talon@gmail.com>
2026-09-07 18:20:12 +0200

    ~~ mode is silly, get rid of it

--- src/jackknify/core/realise.py
+++ src/jackknify/core/realise.py
@@ -11,7 +11,6 @@ def realise(
     col: str,
     n_samples: int,
     seed: int,
-    mode: str,
     out_dir: str | os.PathLike | None = None,
 ):
     """Generates jackknife noise realisations from an MS."""
@@ -21,22 +20,11 @@ def realise(
     print(f"Reading {col} from {ms_file}...")
     original_data = wrapper.get_data(col)
 
-    if mode == "copy" and out_dir:
-        if not os.path.exists(out_dir):
-            os.makedirs(out_dir)
-
     for i in tqdm(range(n_samples), desc="Generating realisations"):
         current_seed = seed + i
         jacked_data = jax_apply_flips(original_data, current_seed)
 
-        if mode == "column":
-            out_col_name = f"{col}_JACK_{i}"
-            wrapper.write_column(out_col_name, jacked_data, desc_template_col=col)
-
-        elif mode == "copy" and out_dir:
-            ms_name = os.path.basename(ms_file).replace(".ms", "")
-            new_ms_path = os.path.join(out_dir, f"{ms_name}_JACK_{i}.ms")
-            new_wrapper = wrapper.create_copy(new_ms_path)
-            new_wrapper.write_column("DATA", jacked_data, desc_template_col=col)
+        out_col_name = f"{col}_JACK_{i}"
+        wrapper.write_column(out_col_name, jacked_data, desc_template_col=col)
 
     print("Done.")
